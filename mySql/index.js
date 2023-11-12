75280f99ef72ade099e25ab16842d8ab44a7f3b9
chananelaze <azenkot.chananel@gmail.com>
2023-11-12 21:16:32 +0200

    added

--- mySql/index.js
+++ mySql/index.js
@@ -8,17 +8,85 @@ const con = mysql.createConnection({
   user: "root",
   password: "root",
   database: "MySqL-Chananel",
-  port:8889
+  port: 8889,
 });
 
-con.connect((err)=>{
-    if(err) {
-        throw err;
+con.connect((err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log("Connected to database");
+});
+
+app.listen(2000, () => {
+  console.log("Server started on port 2000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//get all users
+app.get("/users", (req, res) => {
+  con.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      return res.status(500).send("we have a problem");
     }
-
-    console.log("Connected to database");
+    res.send(result);
+  });
 });
 
-app.listen(2000,()=>{
-    console.log("Server started on port 2000");
+// /users/limit?start=100&quantity=200 = get 200 users from 100
+app.get("/users/limit", (req, res) => {
+  if (!req.query.start) {
+    return res.status(403).send("limit start is missing");
+  }
+  if (!req.query.quantity) {
+    return res.status(403).send("limit quantity is missing");
+  }
+  con.query(
+    "SELECT * FROM `users` LIMIT ? , ?",
+    [+req.query.start, +req.query.quantity],
+    (err, result) => {
+      if (err) {
+        return res.status(500).send("we have a problem");
+      }
+      res.send(result);
+    }
+  );
 });
+
+
+//get user by id
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  con.query("SELECT * FROM `users` WHERE `id` =?", [id], (err, result) => {
+    if (err) {
+      return res.status(502).send("no card signal");
+    }
+    res.send(result.pop());
+  });
+});
+
+//get all cars
+app.get("/carsModel", (req, res) => {
+  con.query("SELECT * FROM carsModel", (err, result) => {
+    if (err) {
+      return res.status(500).send("we have a problem");
+    }
+    res.send(result);
+  });
+});
+
+//get car by id
+app.get("/carsModel/:id", (req, res) => {
+  const id = req.params.id;
+  con.query("SELECT * FROM `carsModel` WHERE `id` =? ", [id], (err, result) => {
+    if (err) {
+      return res.status(502).send("no card signal");
+    }
+    res.send(result.pop());
+  });
+});
+
