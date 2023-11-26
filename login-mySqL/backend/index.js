a9183f726a4c0f1b5457bae0fd6b130a4a31cad8
chananelaze <azenkot.chananel@gmail.com>
2023-11-26 21:00:18 +0200

    added

--- login-mySqL/backend/index.js
+++ login-mySqL/backend/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+require('./sqlConnection');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  session({
+    secret: "my-secret",
+    name: "mySession",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(cors({
     origin: true,
@@ -14,6 +25,15 @@ app.use(cors({
 
 app.listen(4000, () => {
     console.log('Server listening on port 4000');
+});
+
+app.use((req, res, next) => {
+    if(!req.session.visit){
+        req.session.visit = 0;
+    }
+    req.session.visit++;
+    console.log(req.session.visit);
+    next();
 });
 
 app.get('/', (req, res) => {
