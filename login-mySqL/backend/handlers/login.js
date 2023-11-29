e69747c207a4593af713badc83dea9b683eacbd4
chananelaze <azenkot.chananel@gmail.com>
2023-11-29 20:18:50 +0200

    added

--- login-mySqL/backend/handlers/login.js
+++ login-mySqL/backend/handlers/login.js
@@ -4,6 +4,10 @@ module.exports = (app) => {
   app.post("/login", (req, res) => {
     const { email, password } = req.body;
 
+    if (req.session.attempts >= 5) {
+      return res.status(403).send({ message: "user is blocked !! " });
+    }
+
     if (!email || !password) {
       return res.status(400).send({ message: "Some not good " });
     }
@@ -14,6 +18,20 @@ module.exports = (app) => {
         if (err) {
           return res.status(500).send({ message: err.message });
         }
+
+        if (!result.length) {
+          if (!req.session.attempts) {
+            req.session.attempts = 0;
+          }
+          req.session.attempts++;
+
+          return res
+            .status(403)
+            .send({ message: "Email or password is not correct" });
+        }
+
+        delete req.session.attempts;
+
         if (!result.length) {
           return res.status(401).send({ message: "User not found " });
         }
@@ -27,10 +45,10 @@ module.exports = (app) => {
   });
 
   app.get("/login", (req, res) => {
-    if(req.session.user){
-        res.send(req.session.user);
-    }else{
-        res.status(401).send({message: "User not found"});
+    if (req.session.user) {
+      res.send(req.session.user);
+    } else {
+      res.status(401).send({ message: "User not found" });
     }
   });
 };
