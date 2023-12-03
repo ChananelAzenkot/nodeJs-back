600bf8445e2bfc57abb6c38141dbef95ed0bd6ac
chananelaze <azenkot.chananel@gmail.com>
2023-12-03 21:17:42 +0200

    added

--- login-mySqL/frontend/src/components/Dashboard.jsx
+++ login-mySqL/frontend/src/components/Dashboard.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import './Dashboard.css';
 
 export default function Dashboard() {
+  const [structure, setStructure] = useState([
+    { name: "", title: "counter products" },
+    { name: "", title: "mid Prices" },
+    { name: "", title: "high Prices" },
+    { name: "", title: "low Prices" },
+  ]);
+
+  useEffect(() => {
+    const url = "http://localhost:4000/dashboard";
+    const params = {
+      credentials: "include",
+    };
+    Promise.all([
+      fetch(`${url}/products/amount`, params).then((res) => res.text()),
+      fetch(`${url}/products/avg`, params).then((res) => res.text()),
+      fetch(`${url}/products/min`, params).then((res) => res.text()),
+      fetch(`${url}/products/max`, params).then((res) => res.text()),
+    ]).then((data) => {
+      structure.forEach((item, i) => item.value = Math.round(data[i] * 10) / 10);
+      setStructure([...structure]);
+    });
+  }, []);
   return (
-    <div>Dashboard</div>
+    <div className='Dashboard'>
+      {
+        structure.map((s, index) => 
+          <div key={index} className='card'>
+            <header>{s.title}</header>
+            <div>{s.value}</div>
+          </div>    
+        )
+      }
+    </div>
   )
 }
