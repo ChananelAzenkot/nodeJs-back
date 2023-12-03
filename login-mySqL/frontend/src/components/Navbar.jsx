335d3416cd9c079d1d2435b7a16763fbab91259e
chananelaze <azenkot.chananel@gmail.com>
2023-12-03 19:56:18 +0200

    added

--- login-mySqL/frontend/src/components/Navbar.jsx
+++ login-mySqL/frontend/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
+import { useEffect } from 'react';
 import './Navbar.css';
 import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 function Navbar() {
+    
+    const location = useLocation();
+
     return (
         <nav>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/products">Manger Products </Link></li>
+                <li className={location.pathname === '/' ? 'active' : ''}><Link to="/">Home</Link></li>
+                <li className={location.pathname === '/products' ? 'active' : ''}><Link to="/products">Manger Products </Link></li>
             </ul>
         </nav>
     );
