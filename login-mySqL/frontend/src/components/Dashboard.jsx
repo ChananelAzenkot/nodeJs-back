ae3bb651302308c0b2b2dd89a9dcc608b9f4b5ea
chananelaze <azenkot.chananel@gmail.com>
2023-12-03 21:27:39 +0200

    added

--- login-mySqL/frontend/src/components/Dashboard.jsx
+++ login-mySqL/frontend/src/components/Dashboard.jsx
@@ -3,10 +3,10 @@ import './Dashboard.css';
 
 export default function Dashboard() {
   const [structure, setStructure] = useState([
-    { name: "", title: "counter products" },
-    { name: "", title: "mid Prices" },
-    { name: "", title: "high Prices" },
-    { name: "", title: "low Prices" },
+    { name: "amount", title: "counter products" },
+    { name: "avg", title: "mid Prices" },
+    { name: "max", title: "high Prices" },
+    { name: "min", title: "low Prices" },
   ]);
 
   useEffect(() => {
@@ -17,8 +17,8 @@ export default function Dashboard() {
     Promise.all([
       fetch(`${url}/products/amount`, params).then((res) => res.text()),
       fetch(`${url}/products/avg`, params).then((res) => res.text()),
-      fetch(`${url}/products/min`, params).then((res) => res.text()),
       fetch(`${url}/products/max`, params).then((res) => res.text()),
+      fetch(`${url}/products/min`, params).then((res) => res.text()),
     ]).then((data) => {
       structure.forEach((item, i) => item.value = Math.round(data[i] * 10) / 10);
       setStructure([...structure]);
