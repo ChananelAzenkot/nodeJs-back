da298902ee0b9f92792858264bf5ab7d87a5e30d
chananelaze <azenkot.chananel@gmail.com>
2023-12-27 18:57:54 +0200

    added

--- hello-mongoDB/backend/handlers/products/products.js
+++ hello-mongoDB/backend/handlers/products/products.js
@@ -1,59 +1,100 @@
 const guard = require("../../guard");
 const { Product } = require("./products.model");
 
-module.exports = app => {
-    app.get('/products', guard, async (req, res) => {
-        res.send(await Product.find());
-    });
+module.exports = (app) => {
+  app.get("/products", guard, async (req, res) => {
+    res.send(await Product.find());
+  });
 
-    app.get('/products/:id', guard, async (req, res) => {
-        const product = await Product.findOne({ _id: req.params.id });
+  app.get("/products/:id", guard, async (req, res) => {
+    const product = await Product.findOne({ _id: req.params.id });
 
-        if (!product) {
-            return res.status(403).send("Product not found");
-        }
+    if (!product) {
+      return res.status(403).send("Product not found");
+    }
 
-        res.send(product);
-    });
+    res.send(product);
+  });
 
-    app.post('/products', guard, async (req, res) => {
-        const { name, price, discount } = req.body;
+  app.post("/products", guard, async (req, res) => {
+    const { name, price, discount } = req.body;
 
-        if (!name || !price || !discount) {
-            return res.status(403).send("required parameters missing");
-        }
+    if (!name || !price || !discount) {
+      return res.status(403).send("required parameters missing");
+    }
 
-        const product = new Product({ name, price, discount });
-        const obj = await product.save();
+    const product = new Product({ name, price, discount });
+    const obj = await product.save();
 
-        res.send(obj);
-    });
-    
-    app.put("/products/:id", guard, async (req, res) => {
-        const { name, price, discount } = req.body;
+    res.send(obj);
+  });
 
-        if (!name || !price || !discount) {
-            return res.status(403).send("required parameters missing");
-        }
+  app.put("/products/:id", guard, async (req, res) => {
+    const { name, price, discount } = req.body;
 
-        const product = await Product.findOne({ _id: req.params.id });
+    if (!name || !price || !discount) {
+      return res.status(403).send("required parameters missing");
+    }
 
-        if (!product) {
-            return res.status(403).send("Product not found");
-        }
+    const product = await Product.findOne({ _id: req.params.id });
 
-        product.name = name;
-        product.price = price;
-        product.discount = discount;
+    if (!product) {
+      return res.status(403).send("Product not found");
+    }
 
-        product.save();
+    product.name = name;
+    product.price = price;
+    product.discount = discount;
 
-        res.send();
-    });
+    product.save();
 
-    app.delete("/products/:id", guard, async (req, res) => {
-        await Product.deleteOne({ _id: req.params.id });
+    res.send();
+  });
 
-        res.send();
-    });
-}
+  app.delete("/products/:id", guard, async (req, res) => {
+    await Product.deleteOne({ _id: req.params.id });
+
+    res.send();
+  });
+
+  app.get("/dashboard/products/amount", guard, async (req, res) => {
+    const amount = await Product.countDocuments();
+    res.send(amount.toString());
+  });
+
+  app.get("/dashboard/products/avg", guard, async (req, res) => {
+    const avg = await Product.aggregate([
+      {
+        $group: {
+          _id: null,
+          avg: { $avg: "$price" },
+        },
+      },
+    ]);
+    res.send(avg[0].avg.toString());
+  });
+
+  app.get("/dashboard/products/min", guard, async (req, res) => {
+    const min = await Product.aggregate([
+      {
+        $group: {
+          _id: null,
+          min: { $min: "$price" },
+        },
+      },
+    ]);
+    res.send(min[0].min.toString());
+  });
+
+  app.get("/dashboard/products/max", guard, async (req, res) => {
+    const max = await Product.aggregate([
+      {
+        $group: {
+          _id: null,
+          max: { $max: "$price" },
+        },
+      },
+    ]);
+    res.send(max[0].max.toString());
+  });
+};
--- hello-mongoDB/frontend/src/components/Dashboard.js
+++ hello-mongoDB/frontend/src/components/Dashboard.js
@@ -12,14 +12,18 @@ export default function Dashboard() {
     useEffect(() => {
         const url = 'http://localhost:4000/dashboard';
         const params = {
-            credentials: 'include',
+          credentials: "include",
+          headers: {
+            "Content-type": "application/json",
+            Authorization: localStorage.token,
+          },
         };
 
         Promise.all([
             fetch(`${url}/products/amount`, params).then(res => res.text()),
             fetch(`${url}/products/avg`, params).then(res => res.text()),
-            fetch(`${url}/products/min`, params).then(res => res.text()),
             fetch(`${url}/products/max`, params).then(res => res.text()),
+            fetch(`${url}/products/min`, params).then(res => res.text()),
         ])
         .then(data => {
             structure.forEach((item, i) => item.value = Math.round(data[i] * 10) / 10);
