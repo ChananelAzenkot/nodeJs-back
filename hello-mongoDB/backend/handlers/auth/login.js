df4a79997f4e88ecdd442d520ce3c325add673a2
chananelaze <azenkot.chananel@gmail.com>
2024-01-03 20:23:46 +0200

    added

--- hello-mongoDB/backend/handlers/auth/login.js
+++ hello-mongoDB/backend/handlers/auth/login.js
@@ -1,41 +1,76 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { User } = require('./user.model');
-const { JWT_SECRET, getUser } = require('../../config');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("./user.model");
+const { JWT_SECRET, getLoggedUserId } = require("../../config");
 const guard = require("../../guard");
 
-module.exports = app => {
-    app.post('/login', async (req, res) => {
-        const { email, password } = req.body;
+module.exports = (app) => {
+  app.post("/users/login", async (req, res) => {
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(403).send("Inputs can't be empty");
-        }
+    if (!email || !password) {
+      return res.status(403).send("Inputs can't be empty");
+    }
 
-        const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-        if (!user) {
-            return res.status(403).send("email or password is incorrect");
-        }
+    if (!user) {
+      return res.status(403).send("email or password is incorrect");
+    }
 
-        const passwordMatch = await bcrypt.compare(password, user.password);
+    const passwordMatch = await bcrypt.compare(password, user.password);
 
-        if (!passwordMatch) {
-            return res.status(403).send("email or password is incorrect");
-        }
+    if (!passwordMatch) {
+      return res.status(403).send("email or password is incorrect");
+    }
 
-        const userObject = user.toObject();
-        delete userObject.password;
-        delete userObject.email;
-
-        userObject.token = jwt.sign({ user: userObject }, JWT_SECRET, { expiresIn: '1h' });
-
-        res.send(userObject);
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "1h",
     });
 
-    app.get('/login', guard, async (req, res) => {
-        const user = getUser(req, res);
+    res.send(token);
+  });
 
-        res.send(user);
-    });
-}
+  app.get("/users", guard, async (req, res) => {
+    const users = await User.find().select("-password");
+
+    res.send(users);
+  });
+
+  app.get("/users/:id", guard, async (req, res) => {
+    const userId = getLoggedUserId(req, res);
+    const user = await User.findById(userId);
+
+    if (userId !== req.params.id && !user?.isAdmin) {
+      return res.status(401).send("User not authorized");
+    }
+
+    try {
+      const user = await User.findById(req.params.id).select("-password");
+
+      if (!user) {
+        return res.status(403).send("User not found");
+      }
+
+      res.send(user);
+    } catch (err) {
+      return res.status(403).send("User not found");
+    }
+  });
+
+  app.patch("/users/:id", guard, async (req, res) => {
+    const userId = getLoggedUserId(req, res);
+
+    if (userId !== req.params.id) {
+      return res.status(401).send("User not authorized");
+    }
+
+    const user = await User.findById(req.params.id);
+
+    user.isBusiness = !user.isBusiness;
+
+    await user.save();
+
+    res.end();
+  });
+};
--- hello-mongoDB/backend/handlers/auth/user.model.js
+++ hello-mongoDB/backend/handlers/auth/user.model.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    fullName: String,
-    email: String,
-    password: String,
+  fullName: String,
+  email: String,
+  password: String,
+  isBusiness: { type: Boolean, default: false },
 });
 
 exports.User = mongoose.model("users", schema);
